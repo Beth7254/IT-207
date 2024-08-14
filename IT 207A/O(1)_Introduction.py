6ae9e13c7b2c31a69e93fbb5ebdb90f1344d5454
MARY BETH GRACIA <143368733+Beth7254@users.noreply.github.com>
2024-08-14 17:05:36 +0800

    new commit

---
+++ IT 207A/O(1)_Introduction.py
@@ -0,0 +1,5 @@
+arr = [1, 3, 5, -3, 9, -6, 10, 11, 30, 16] #the given array 
+index = 6 #the counting value of 6 which is 10
+value = arr[index] 
+
+print("The Specific Value of this Array is", value) # 
